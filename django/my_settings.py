8efba38f497f0e9b499e6397cfde8b4ecfcf890f
hasangjae <dlbia2009@gmail.com>
2023-06-04 19:57:37 +0900

    일복 프로젝트 업로드

---
+++ django/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db명',
+        'USER': '사용자 이름',
+        'PASSWORD': '비밀번호',
+        'HOST': 'host 명',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-jz4ioly_)cu0q&(2p+jm#ksvse)5k#3uwid%l6f9!)auanpkv5'
